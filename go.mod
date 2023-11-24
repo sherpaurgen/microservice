3fb187460ccd8e61d17409e13958e4d02a2563de
urgen sherpa <sherpaurgen@users.noreply.github.com>
2023-11-25 01:19:33 +0545

    add loggers

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/sherpaurgen/microservice
 
 go 1.21.4
+
+require (
+	github.com/go-chi/chi/v5 v5.0.10 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "net/http"
+import (
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"go.uber.org/zap"
+)
 
 func main() {
-	http.ListenAndServe(":9090", nil)
+	logger, err := zap.NewProduction()
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(rw http.ResponseWriter, r *http.Request) {
+		logger.Info("Get received at /")
+		dataBody, _ := io.ReadAll(r.Body)
+		logger.Info("Data received", zap.ByteString("body", dataBody))
+	})
+
+	if err != nil {
+		log.Fatalf("Cannot initialize zap logger %v", err)
+	}
+
+	defer logger.Sync()
+	http.ListenAndServe(":9090", r)
 }
